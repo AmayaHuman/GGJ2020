f27ca9f8c605045cad0e54e8f4a6ea76d13a98f1
Bebo-K <Bebo.Kimble@gmail.com>
2020-02-01 00:30:29 -0500

    Stick that destroys brushes

---
+++ source/LostWoods/Assets/Scripts/Brush.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brush : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        print("ooh");
+        if (other.name == "Stick")
+        {
+            GameObject.Destroy(this.gameObject, 0.0f);
+        }
+    }
+
+    void OnTriggerExit(Collider other) { }
+
+}
