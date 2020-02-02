4bdb4a458fdc38b9fb7f36090efd8a873add4b33
Bebo-K <Bebo.Kimble@gmail.com>
2020-02-02 02:27:04 -0500

    More prefabs for wind-based activities

---
+++ source/LostWoods/Assets/Scripts/WindWave.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindWave : MonoBehaviour
+{
+    private Vector3 initialPoint;
+
+    private float restart_timer;
+    public float restart_time;
+    public float wave_speed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialPoint = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        restart_timer += Time.deltaTime;
+        transform.position += transform.forward * Time.deltaTime * wave_speed;
+        if (restart_timer > restart_time) {
+            transform.position = initialPoint;
+            restart_timer -= restart_time;
+        }
+
+    }
+
+
+
+
+
+}
